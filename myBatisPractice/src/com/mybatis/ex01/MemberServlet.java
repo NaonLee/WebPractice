425ff26d712716769f2eec56f53114f69812f90a
NaonLee <giraffe1254@gmail.com>
2020-12-28 23:09:02 +0900

    Mybatis-READ

---
+++ myBatisPractice/src/com/mybatis/ex01/MemberServlet.java
@@ -0,0 +1,40 @@
+package com.mybatis.ex01;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/mem.do")
+public class MemberServlet extends HttpServlet {
+
+    public MemberServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> membersList = dao.selectAllMembers();
+		request.setAttribute("membersList", membersList);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("test01/listMembers.jsp");
+		dispatcher.forward(request, response);
+	}
+}
