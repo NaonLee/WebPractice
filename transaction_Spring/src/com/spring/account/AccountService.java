f0f378bc25e7afc4234ceeeef132418117112fd4
NaonLee <giraffe1254@gmail.com>
2020-12-30 00:44:16 +0900

    TRANSACTION-Spring practice

---
+++ transaction_Spring/src/com/spring/account/AccountService.java
@@ -0,0 +1,18 @@
+package com.spring.account;
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(propagation=Propagation.REQUIRED)
+public class AccountService {
+	private AccountDAO accDAO;
+	
+	public void setAccDAO(AccountDAO accDAO) {
+		this.accDAO = accDAO;
+	}
+	
+	public void sendMoney() {
+		accDAO.updateBalance1();
+		accDAO.updateBalance2();
+	}
+}
